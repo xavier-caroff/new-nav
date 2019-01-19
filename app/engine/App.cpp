f600fdffd6d1f491459e7847b90d4fde8f808133
Xavier Caroff <xavier.caroff@ixblue.com>
2019-01-19 17:23:29 +0100

    Add command line argument to get the config file name

--- app/engine/App.cpp
+++ app/engine/App.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
 
 #include "App.h"
+
+namespace bpo = boost::program_options;
 
 namespace newNav {
 namespace app {
@@ -27,43 +30,147 @@ int App::execute(
 	int		argc,
 	char*	argv[])
 {
-	int		result{ 0 };
+	int					result{ 0 };
+	CommandLineOptions	options;
+	bool				startEngine = true;
 
-	// Load the configuration file.
+	// Read command line options.
 
 	if (result == 0)
 	{
-		std::ifstream	file("/home/xca/DEV/new-nav/my-config.json");
+		try
+		{
+			parseCmdLine(argc, argv, options);
+		}
+		catch(std::exception&	e)
+		{
+			std::cerr << "command line error: " << e.what() << std::endl;
+			result = 1;
+		}
+	}
 
-		file >> _config;
+	// Show help eventually.
+
+	if (result == 0 && options.printHelp)
+	{
+		// Help message already displayed by parseCmdLine().
+		startEngine = false;
+	}
+
+	// Start the engine
+
+	if (result == 0 && startEngine)
+	{
+		start(options);
+	}
+
+	return result;
+}
+
+// Parse the options from the command line.
+void	App::parseCmdLine(
+	int					argc,
+	char*				argv[],
+	CommandLineOptions&	options)
+{
+	// Description of general options
+
+	bpo::options_description	general("General options");
+
+	general.add_options()
+		("help,h",		"Print usage information and exit");
+
+	// Description of runtime options
+
+	bpo::options_description	runtime("Runtime options");
+
+	runtime.add_options()
+		("config,c", bpo::value<boost::filesystem::path>(&options.configFile)->required(),		"Specify the configuration file");
+
+	// Description des options visibles
+
+	bpo::options_description	visible("Allowed options");
+
+	visible.add(general).add(runtime);
+
+	// Description of all options supported on the command line
+
+	bpo::options_description	all("Allowed options");
+	
+	all.add(general).add(runtime);
+
+	// Parse command line
+
+	bpo::variables_map			vm;
+
+	bpo::store(bpo::parse_command_line(argc, argv, all), vm);
+
+	bpo::notify(vm);
+
+	// Analyze parsed values
+
+	options.printHelp = vm.count("help") != 0;
+
+	// Print help if required
+
+	if (options.printHelp)
+	{
+		std::cout << "Usage" << std::endl;
+		std::cout << "    new-nav-engine [options] --config <file>" << std::endl << std::endl;
+
+		std::cout << general << std::endl;
+
+		std::cout << runtime << std::endl;
+	}
+}
+
+// Start the engine
+void	App::start(
+	const CommandLineOptions&	options)
+{
+	bool	goOn{ true };
+
+	// Load the configuration file.
+
+	if (goOn)
+	{
+		std::ifstream		file;
+		
+		file.exceptions(std::ios::failbit);
+
+		try
+		{
+			file.open(options.configFile.string().c_str());
+			file >> _config;
+		}
+		catch(std::exception&	e)
+		{
+			std::cerr << "failed to read config file: " << e.what() << std::endl;
+			goOn = false;
+		}
 	}
 
 	// Load the modules located into the executable folder and additional folder
 	// provided into the configuration.
 
-	if (result == 0)
+	if (goOn)
 	{
 		loadModules();
 	}
 
 	// Create the components.
 
-	if (result == 0)
+	if (goOn)
 	{
-		if (createComponents() == false)
-		{
-			result = 1;
-		}
+		goOn = createComponents();
 	}
 
 	// Configure the components.
 
-	if (result == 0)
+	if (goOn)
 	{
 		configureComponents();
 	}
-
-	return result;
 }
 
 // Load all possible modules.
--- app/engine/App.h
+++ app/engine/App.h
@@ -32,16 +32,49 @@ public:
 public:
 
 	/// Launch the application.
+	///
+	/// @param argc Command line argument count.
+	/// @param argv Command line arguments.
+	///
+	/// @return The exit code to pass to the operating system.
 	int execute(
 		int		argc,
 		char*	argv[]);
 
+// Definition
+private:
+
+	/// Command line options
+	struct CommandLineOptions
+	{
+		/// Print usage information.
+		bool					printHelp{ false };
+		/// Configuration file name.
+		boost::filesystem::path	configFile;
+	};
+
 // Implementation
 private:
 
+	/// Parse the options from the command line.
+	///
+	/// @param argc Command line argument count.
+	/// @param argv Command line arguments.
+	/// @param options [out] Options read from the command line.
+	void	parseCmdLine(
+		int					argc,
+		char*				argv[],
+		CommandLineOptions&	options);
+
+	/// Start the engine
+	///
+	/// @param options Options read from the command line.
+	void	start(
+		const CommandLineOptions&	options);
+
 	/// Load all possible modules.
 	void	loadModules(
-	);
+		);
 
 	/// Load the external modules located into a given folder.
 	///
